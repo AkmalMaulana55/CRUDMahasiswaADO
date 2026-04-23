8e955667d2d52d10752af438275b6e02ff96785a
niel55-stack <vylingsniel55@gmail.com>
2026-04-23 14:21:57 +0700

    menambahkan koneksi ke sql

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -13,6 +13,9 @@ namespace CRUDMahasiswaADO
 {
     public partial class Form1: Form
     {
+        private readonly SqlConnection conn;
+        private readonly string connectionString =
+        "Data Source=MYBOOKHYPE\\AKMALSQL; Initial Catalog=DBAkademikADO; Integrated Security=True";
         public Form1()
         {
             InitializeComponent();
