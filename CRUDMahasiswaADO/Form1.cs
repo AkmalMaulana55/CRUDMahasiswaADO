e45dc83abc56b3ca76969336cc377ab5f1be5600
niel55-stack <vylingsniel55@gmail.com>
2026-04-23 14:25:55 +0700

    mengisi public form

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -19,6 +19,7 @@ namespace CRUDMahasiswaADO
         public Form1()
         {
             InitializeComponent();
+            conn = new SqlConnection(connectionString);
         }
 
         private void label3_Click(object sender, EventArgs e)
